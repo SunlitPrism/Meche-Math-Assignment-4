cbe7733592bc0331db1eb3216f39b6428125e63f
SunlitPrism <rand.avery.mosley@gmail.com>
2025-10-17 17:09:00 -0400

    End of Class 10/17

--- assignment_4_setup.m
+++ assignment_4_setup.m
@@ -10,9 +10,9 @@ function assignment_4_setup()
     % orbit_params.m_planet =
     % orbit_params.G = 6.6743e-11; %m3kg-1 s-2
 
-    rate_func_in = @rate_func02; %gravity_rate_func;
+    XA = [x_p];
+    rate_func_in = @rate_func01; %gravity_rate_func;
 
-    XA = [x_p,y_p];
     h = 0.1;
 
 
@@ -23,8 +23,5 @@ function assignment_4_setup()
 
     disp(BT_struct)
     
-    [XB, num_evals] = explicit_RK_step(rate_func_in,t,XA,h,BT_struct);
-
-    disp(XB)
-    num_evals(XB)
+    [XB, num_evals] = explicit_RK_step(rate_func_in,t,XA,h,BT_struct)
 end
