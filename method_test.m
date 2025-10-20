3403a53cb024f5c6fd780c6a83120042986a8174
lodio <lodio@olin.edu>
2025-10-20 09:09:47 -0400

    Intermediate work

---
+++ method_test.m
@@ -0,0 +1,9 @@
+%%
+
+% choose three or more different explicit Runge-Kutta methods to test
+
+% Ideally, there will be a few different orders of truncation error that 
+% are represented from among the methods that you chose.
+
+% Perform a set of experiments comparing the behavior of the different method
+
