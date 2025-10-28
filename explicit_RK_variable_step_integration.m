09d48f88e315446390de80c5beeec0865d501ac4
SunlitPrism <rand.avery.mosley@gmail.com>
2025-10-28 16:21:57 -0400

    Variable step integration implemented

--- explicit_RK_variable_step_integration.m
+++ explicit_RK_variable_step_integration.m
@@ -16,5 +16,44 @@
 %h_avg: the average step size
 %num_evals: total number of calls made to rate_func_in during the integration
 function [t_list,X_list,h_avg, num_evals] = explicit_RK_variable_step_integration(rate_func_in,tspan,X0,h_ref,BT_struct,p,error_desired)
-%your code here
+    N = ceil(diff(tspan)/h_ref)+1; % add 1 bc of inclusivity in linspace range
+
+    t_list = linspace(tspan(1), tspan(2), N);
+    X_list = zeros(N, length(X0));
+
+    h_next_list = [];
+    
+    h = diff(t_list(1:2));  
+    
+    total_evals = 0; % counter for num times rate func is called
+
+    % start with inital condition, calc ans store future vals
+    X_list(1, :) = X0;
+    redo = FALSE;
+
+    for i = 1:N-1
+        if redo == TRUE
+            disp("redo iter")
+            % select t val, find x_(i+1), update x_i (XA here)
+            t = t_list(i);
+            [XB, num_evals, h_next, redo] = explicit_RK_variable_step(rate_func_in,t,XA,h,BT_struct,p,error_desired);
+            h_next_list(i) = h_next;
+            h = h_next;
+            total_evals = total_evals + num_evals;
+
+        elseif redo == FALSE
+            % select t val, find x_(i+1), update x_i (XA here)
+            t = t_list(i);
+            [XB, num_evals, h_next, redo] = explicit_RK_variable_step(rate_func_in,t,XA,h,BT_struct,p,error_desired);
+            h_next_list(i) = h_next;
+            h = h_next;
+
+            % update and store vals
+            X_list(i+1, :) = XB(1,:); %Saving just XB1 
+            total_evals = total_evals + num_evals;
+        else
+            disp('error')
+        end
+    end
+    h_avg = mean(h_next_list);
 end
